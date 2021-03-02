f0c276200b3a476053cbc481a70c0899ca8ded05
fitzgebt <fitzgebt@gmail.com>
2021-03-02 11:05:57 -0800

    created get/post routes for '/signup' in teacher_controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,19 @@
 class TeacherController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+            redirect '/assignments'
+        end
+        erb :'teachers/new'
+    end
+
+    post '/signup' do
+        if Teacher.create(params).id
+            session[:teacher_id] = Teacher.last.id
+            redirect '/assignments'
+        end
+        redirect '/signup' 
+    end
     get '/teachers' do
       @teachers = Teacher.all
       erb :'/teachers/index'
@@ -9,6 +23,8 @@ class TeacherController < ApplicationController
       @teacher = teacher.find_by_slug(params[:slug])
       erb :'/teachers/show'
     end
+
+
   
 end
   
