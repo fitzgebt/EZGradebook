875d31d741475d98c9de8a137d607648c4cdddcf
fitzgebt <fitzgebt@gmail.com>
2021-03-02 13:01:35 -0800

    AssignmentController - created get/assignments, get/post routes for 'assignments/new.erb'.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,5 +1,29 @@
 class AssignmentsController < ApplicationController
 
+    get '/assignments' do
+        if !logged_in?
+            redirect '/login'
+        end
+        @assignments = Assignment.all
+        erb :'/assignments/index'
+    end
+    
+    get '/assignments/new' do 
+        if !logged_in?
+            redirect '/login'
+        end
+        erb :'/assignments/new'
+    end
+
+    post '/assignments' do
+        if Assignment.create(params).id
+            @assignment = Assignment.all.last
+            @assignment.teacher_id = current_user.id
+            @assignment.save
+            redirect '/assignments/#{@assignment.id}'
+        end
+        redirect '/assignments/new'
+    end
     
 
 end
