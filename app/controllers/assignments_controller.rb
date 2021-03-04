9e7abc9c06f4bc86a7d47023c665678663a4d620
fitzgebt <fitzgebt@gmail.com>
2021-03-04 13:15:53 -0800

    added link funtionality to 'students/index' and 'students/show' to display and link assignment/grade info

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,35 +3,33 @@ class AssignmentsController < ApplicationController
     get '/assignments' do
         if !logged_in?
             redirect '/login'
-        else
-            @assignments = Assignment.all
-            erb :'/assignments/index'
-        end
+        end        
+        @assignments = Assignment.all
+        erb :'/assignments/index'
     end
 
     
     get '/assignments/new' do 
         if !logged_in?
             redirect '/login'
-        else
+        end 
             @students = Student.all
             erb :'/assignments/new'
-        end
     end
 
     post '/assignments' do
         if Assignment.create(title: params[:assignment][:title], content: params[:assignment][:content]).id
-            @assignment = Assignment.all.last
-            @assignment.teacher_id = current_user.id
-            @students = Student.all
-            @students.each do |student|
-                AssignmentsStudent.create(assignment_id: @assignment.id, student_id: student.id)
+            assignment = Assignment.all.last
+            assignment.teacher_id = current_user.id
+            students = Student.all
+            students.each do |student|
+                AssignmentsStudent.create(assignment_id: assignment.id, student_id: student.id)
             end
-            @assignment.assignments_students.each do |assignment_student|
+            assignment.assignments_students.each do |assignment_student|
                 assignment_student.update(grade: params[:students][assignment_student.student_id.to_s][:grade])
             end           
-            @assignment.save
-            redirect "/assignments/#{@assignment.id}"
+            assignment.save
+            redirect "/assignments/#{assignment.id}"
         else
             redirect '/assignments/new'
         end
@@ -52,28 +50,28 @@ class AssignmentsController < ApplicationController
     end
 
     get '/assignments/:id' do
-        if logged_in?
-            @assignment = Assignment.find_by_id(params[:id])
-            @teacher = Teacher.find_by_id(@assignment.teacher_id)
-            erb :'/assignments/show'
-        else
+        if !logged_in?
             redirect '/teachers/login'
         end
+        @assignment = Assignment.find_by_id(params[:id])
+        binding.pry
+        @teacher = Teacher.find_by_id(@assignment.teacher_id)
+        erb :'/assignments/show'
     end
 
     patch '/assignments/:id' do
         if !logged_in?
             redirect '/login'
         end
-        @assignment = Assignment.find_by_id(params[:id])
+        assignment = Assignment.find_by_id(params[:id])
         if params[:assignment][:content] == "" || params[:assignment][:title] == ""
-            redirect "assignments/#{@assignment.id}/edit"
+            redirect "assignments/#{assignment.id}/edit"
         else
-            @assignment.update(title: params[:assignment][:title], content: params[:assignment][:content])         
-            @assignment.assignments_students.each do |assignment_student|
+            assignment.update(title: params[:assignment][:title], content: params[:assignment][:content])         
+            assignment.assignments_students.each do |assignment_student|
                 assignment_student.update(grade: params[:students][assignment_student.student_id.to_s][:grade])
             end           
-            redirect "/assignments/#{@assignment.id}"
+            redirect "/assignments/#{assignment.id}"
         end
     end
 
@@ -81,9 +79,9 @@ class AssignmentsController < ApplicationController
         if !logged_in?
             redirect '/login'
         end
-        @assignment = Assignment.find_by_id(params[:id])
-        if current_user.id == @assignment.teacher_id
-            @assignment.destroy
+        assignment = Assignment.find_by_id(params[:id])
+        if current_user.id == assignment.teacher_id
+            assignment.destroy
             redirect "/teachers/#{current_user.slug}"
         else
         redirect "/teachers/#{current_user.slug}"
